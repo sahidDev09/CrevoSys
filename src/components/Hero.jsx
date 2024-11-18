2f9960d547d101fcee0bcac847aa889aa9142d67
sahidDev09 <iamsahidofficial99@gmail.com>
2024-11-19 02:48:24 +0600

    fix ui mismatched

---
+++ src/components/Hero.jsx
@@ -0,0 +1,41 @@
+const Hero = () => {
+  return (
+    <div className="flex flex-col gap-3 justify-center mt-12 container mx-auto">
+      <div className="flex mb-5 items-center justify-center">
+        <div className=" rounded-full bg-gradient-to-r from-[#D9D9D9] to-[#737373] items-center">
+          <div className="flex items-center gap-3 md:py-2 md:px-3 p-2">
+            <img
+              className="w-[30px] h-[30px] rounded-2xl"
+              src="star.png"
+              alt=""
+            />
+            <h1 className="text-black font-semibold text-sm md:text-base">
+              Your #1 Platform for IT solution
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="flex mt-3 items-center justify-center">
+        <h1 className="text-white md:text-8xl text-4xl font-semibold tracking-wide">
+          Cretivity Sparks <br />{" "}
+          <span className="mt-3 items-center justify-center flex">
+            Revolution
+          </span>{" "}
+        </h1>
+      </div>
+      <div className="flex mt-3 items-center justify-center">
+        <p className="text-gray-400 md:text-lg text-center">
+          Revolutionising businesses with modern IT solutions and world-class
+          design.
+        </p>
+      </div>
+      <div className="flex md:mt-10 mt-5 items-center justify-center">
+        <div className="rounded-full bg-gradient-to-r from-[#FFB16B] to-[#996A40] md:p-3 p-2 text-white">
+          <p className="font-semibold text-black">Meet Our Awesome Team</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
-
-function Navbar() {
+const Navbar = () => {
   return (
-
     <div>
-        <div className='flex justify-around items-center '>
-            <img className='w-60 h-32' src="logo.png" alt="" srcset="" />
-            <div className='flex gap-10 cursor-pointer items-center justify-center text-white'>
-                <a href="">Home</a>
-                <a href="">Blog</a>
-                <a href="">About Us</a>
-                <a href="">Team</a>
-            </div>
-            <button className='bg-transparent border-white text-white'>
-                <a href="">Contact Us</a>
-            </button>
+      <div className="md:flex justify-around items-center">
+        <img className="w-44 mx-auto md:mx-0" src="logo.png" alt="" />
+        <div className="flex gap-10 justify-center mb-4 md:mb-0 cursor-pointer items-center text-white">
+          <a href="">Home</a>
+          <a href="" className="hidden md:inline-block">
+            Blog
+          </a>
+          <a href="">About Us</a>
+          <a href="" className="hidden md:inline-block">
+            Projects
+          </a>
+          <a href="">Team</a>
         </div>
+        <div>
+          <button
+            type="submit"
+            className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg  backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 text-white before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-orange-400 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 hover:border-none relative z-10 px-4 py-2 overflow-hidden border  rounded-full group">
+            Contact Us
+            <svg
+              className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 bg-orange-400 ease-linear duration-300 rounded-full border-gray-700 group-hover:border-none p-2 rotate-45"
+              viewBox="0 0 16 19"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+                className="fill-gray-200 group-hover:fill-gray-800"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
