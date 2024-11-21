30895941838e175192b43d5f7a7d2005bf357cfa
sahidDev09 <iamsahidofficial99@gmail.com>
2024-11-21 14:28:52 +0600

    complete: startup card

--- src/components/StartupCard.jsx
+++ src/components/StartupCard.jsx
@@ -1,33 +1,62 @@
+import { ShieldCheck } from "lucide-react";
+
 const StartupCard = () => {
   return (
-    <div>
-      <h1 className=" md:text-6xl text-white text-center my-5">
+    <div className=" m-2 md:m-0 md: pb-10">
+      <h1 className=" md:text-6xl md:pb-5 text-2xl text-white text-center my-5">
         A Talented team <br /> for-your startup
       </h1>
       <div className=" relative">
-        <div className=" absolute z-10 right-0 -bottom-10">
+        <div className=" absolute z-10 right-0 -bottom-10 hidden md:inline">
           <img className=" w-96" src="/public/Right-hand.webp" alt="" />
         </div>
         <div className="relative h-[550px] w-full rounded-2xl container mx-auto bg-gradient-to-b from-[#F6AA68] to-[#90643D] overflow-hidden">
           <div className=" absolute h-96 w-96 bg-gradient-to-b from-[#A542E2] to-[#1BB6EB] rounded-full bg-no-repeat -top-14 -right-14 bg-contain "></div>
           {/* all content */}
           <div className=" absolute w-full p-10">
+            {/* card header */}
             <div className="flex justify-between items-center">
-              <div className=" flex gap-7 items-center">
+              <div className=" flex md:gap-10 items-center">
                 <img
-                  className=" w-36 rounded-full"
+                  className=" md:w-36 w-20 rounded-full"
                   src="/public/Rectangle.png"
                   alt=""
                 />
-                <h1 className=" text-black text-3xl w-[600px] font-semibold">
+                <h1 className=" text-black text-3xl w-[500px] font-semibold hidden md:inline">
                   Our awesome team is creating a specialised marketplace
                   tailored for our clients.
                 </h1>
               </div>
               <div>
-                <button className=" p-3 bg-white rounded-full text-xl font-semibold uppercase text-black">
-                  JOin with us
+                <button className="md:p-3 p-2 bg-white rounded-full md:text-xl font-semibold uppercase text-black">
+                  JOin with team
                 </button>
+              </div>
+            </div>
+            <div className="md:w-[75%] p-4 md:flex gap-14">
+              <div className="flex md:flex-col gap-10 my-4">
+                <div>
+                  <ShieldCheck size={50} color="#ffffff" strokeWidth={1.25} />
+                  <h1 className=" md:text-xl text-sm text-white mt-2">
+                    <span className=" font-bold">CrevoSys</span> team working
+                    for <br /> your agency
+                  </h1>
+                </div>
+                <div>
+                  <ShieldCheck size={50} color="#ffffff" strokeWidth={1.25} />
+                  <h1 className=" md:text-xl text-sm text-white mt-2">
+                    <span className=" font-bold">With more than 2 </span> years
+                    real <br /> experience.
+                  </h1>
+                </div>
+              </div>
+              <div className="text-start mx-auto flex flex-col gap-5 justify-center">
+                <h1 className="md:text-9xl text-6xl font-extrabold text-white">
+                  06
+                </h1>
+                <p className=" md:text-xl text-white">
+                  We have 6+ talented and skilled <br /> team members.
+                </p>
               </div>
             </div>
           </div>
