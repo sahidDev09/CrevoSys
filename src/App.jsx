87a9b5631ff3e286acb96dd08b6fd550fc7d9cd2
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-11-21 14:52:01 +0600

    added email contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
+import Contact from "./components/contact";
 
 function App() {
   return (
-    <div className="bg-gradient-to-t from-[#070707] to-[#221f35]">
-      <Home />
-    </div>
+    <Router>
+      <div className="bg-gradient-to-t from-[#070707] to-[#221f35] h-screen">
+        <Routes>
+          {/* Define route for Home */}
+          <Route path="/" element={<Home />} />
+          {/* Define route for Contact */}
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
