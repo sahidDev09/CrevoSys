c2f91bc9f560b74bf8f3c22bb33040556f95beb7
sahidDev09 <iamsahidofficial99@gmail.com>
2024-11-22 17:08:47 +0600

    added services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,42 @@
+import { MoveRight } from "lucide-react";
+import { useEffect, useState } from "react";
+
 const Services = () => {
+  const [service, setService] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, []);
+
   return (
-    <div className="services min-h-screen w-full border bg-[url('/servicebg.png')]">
-      
+    <div className="relative overflow-hidden services min-h-screen w-full bg-cover bg-no-repeat bg-center bg-[url('/servicebg.png')]">
+      <h1 className="text-center py-14 md:text-5xl text-3xl text-white">
+        Services Web Provide
+      </h1>
+      <div className="container mx-auto grid grid-cols-4 gap-7">
+        {service.map((serviceCard, index) => (
+          <div
+            key={index}
+            className=" bg-[#1B1B1B] p-10 flex flex-col gap-5 rounded-2xl my-10 hover:border border-gray-600 transition-all">
+            <img className=" w-28 h-28" src={serviceCard.icon} alt="" />
+            <h1 className=" text-2xl font-semibold">{serviceCard.title}</h1>
+            <p className=" text-gray-500">{serviceCard.description}</p>
+            <p className=" flex gap-3 items-center group-hover:">
+              Learn more
+              <span className="group">
+                <MoveRight />
+              </span>
+            </p>
+          </div>
+        ))}
+      </div>
+      <img
+        className=" absolute w-36 right-10 -bottom-10"
+        src="/card_icons/Light-image.png"
+        alt=""
+      />
     </div>
   );
 };
--- src/components/StartupCard.jsx
+++ src/components/StartupCard.jsx
@@ -2,24 +2,24 @@ import { ShieldCheck } from "lucide-react";
 
 const StartupCard = () => {
   return (
-    <div className=" m-4 md:m-0 md:pb-20 py-10 bg-[#1D232A]">
+    <div className="m-4 md:m-0 md:pb-20 py-10 md:bg-[#1D232A]">
       <h1 className=" md:text-5xl md:pb-5 text-4xl text-white text-center mb-5 md:mb-5">
         A Talented team <br /> for-your startup
       </h1>
       <div className=" relative">
         <div className=" absolute z-10 right-0 -bottom-10 hidden md:inline">
-          <img className=" w-96" src="/public/Right-hand.webp" alt="" />
+          <img className=" w-96" src="/Right-hand.webp" alt="" />
         </div>
         <div className="relative h-[550px] w-full rounded-2xl container mx-auto bg-gradient-to-b from-[#F6AA68] to-[#90643D] overflow-hidden">
           <div className=" absolute h-96 w-96 bg-gradient-to-b from-[#A542E2] to-[#1BB6EB] rounded-full bg-no-repeat -top-14 -right-14 bg-contain "></div>
           {/* all content */}
-          <div className=" absolute w-full p-10">
+          <div className=" absolute w-full md:p-10 p-5">
             {/* card header */}
             <div className="flex justify-between items-center">
               <div className=" flex md:gap-10 items-center">
                 <img
                   className=" md:w-36 w-20 rounded-full"
-                  src="/public/Rectangle.png"
+                  src="/Rectangle.png"
                   alt=""
                 />
                 <h1 className=" text-black text-3xl w-[500px] font-semibold hidden md:inline">
@@ -28,8 +28,8 @@ const StartupCard = () => {
                 </h1>
               </div>
               <div>
-                <button className="md:p-3 p-2 bg-white rounded-full md:text-xl font-semibold uppercase text-black">
-                  JOin with team
+                <button className="md:p-3 p-2 bg-white rounded-full md:text-xl text-sm font-semibold uppercase text-black">
+                  Join with team
                 </button>
               </div>
             </div>
