af3a9e2b4d2b87396e18f93c0abe3a4a36549f10
sahidDev09 <iamsahidofficial99@gmail.com>
2024-11-22 13:05:34 +0600

    service bg

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,15 +1,7 @@
 const Services = () => {
   return (
-    <div className="min-h-full w-full border bg-[#121212]">
-      <h1 className=" text-4xl text-white text-center py-10">
-        Services We Provide
-      </h1>
+    <div className="services min-h-screen w-full border bg-[url('/servicebg.png')]">
       
-      <div className="services w-full min-h-screen">
-        <h1>Hello hi</h1>
-
-      </div>
-
     </div>
   );
 };
